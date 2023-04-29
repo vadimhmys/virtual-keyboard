0c6c17638d7c456968220386a177539cb3ceb47a
Vadim Hmys <vadimhmyz@gmail.com>
2023-04-29 10:01:47 +0300

    feat: implement the functionality of the Backspace key

--- main.js
+++ main.js
@@ -13,6 +13,14 @@ function printChar(targetClass) {
   textarea.value += char;
 }
 
+function removeCharBefore() {
+  const textarea = document.querySelector('textarea');
+  let { value } = textarea;
+  if (value === '') return;
+  value = value.slice(0, value.length - 1);
+  textarea.value = value;
+}
+
 function prepareVirtualKeyboard() {
   const keyboard = document.querySelector('.keyboard');
   keyboard.addEventListener('mousedown', (e) => {
@@ -21,6 +29,12 @@ function prepareVirtualKeyboard() {
     const targetClass = target.getAttribute('class').split(' ')[1];
     if (!funcKeys.includes(targetClass)) {
       printChar(targetClass);
+    } else {
+      switch (targetClass) {
+        case 'Backspace': removeCharBefore();
+          break;
+        default: removeCharBefore();
+      }
     }
     return undefined;
   });
